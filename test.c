1262fca0be10b269ff38743c3e10ab1e3dc4f3a3
atip chao <atip/chaogunhar@cookcountyil.gov>
2021-04-21 20:03:56 -0500

    top 10 each

--- test.c
+++ test.c
@@ -12,7 +12,7 @@ typedef struct Covidcase_struct {
    int  currVent;
 } Covidcase;
 
-void SelectionSortCase (Covidcase covidState[], int size) {
+void SelectionSortPos (Covidcase covidState[], int size) {
    int i;
    int j;
    int indexSmallest;
@@ -50,8 +50,8 @@ void printCaseData (Covidcase caseC) {
 int main() {
    
    FILE* inFile = NULL;
-   Covidcase covidCase[10000];     //unknow max data
-   Covidcase inCoState[1000];
+   //Covidcase covidCase[10000];     //unknow max data
+   Covidcase inCoState[6000];
    int ns;
    char inputS[10];
    
@@ -82,7 +82,8 @@ int main() {
    
    int i = 0;
    int j = 0;
-   int count = 0;
+   int newPos, newTot, newDeath, currHos, currVent;
+   char date[20], state[20];
    char currS[20];
    int totalPos = 0;
    int totDea = 0;
@@ -90,66 +91,65 @@ int main() {
    while(!feof(inFile)){
       // fscanf(inFile, "%s %s %d %d %d %d %d", covidCase[i].date, covidCase[i].state, &covidCase[i].newPos,
                              //&covidCase[i].newTot, &covidCase[i].newDeath, &covidCase[i].currHos, &covidCase[i].currVent);
-      fscanf(inFile, "%s", covidCase[i].date);
-      fscanf(inFile, "%s", covidCase[i].state);
-      fscanf(inFile, "%d", &covidCase[i].newPos);
-      fscanf(inFile, "%d", &covidCase[i].newTot);
-      fscanf(inFile, "%d", &covidCase[i].newDeath);
-      fscanf(inFile, "%d", &covidCase[i].currHos);
-      fscanf(inFile, "%d", &covidCase[i].currVent);
+      fscanf(inFile, "%s", date);
+      fscanf(inFile, "%s", state);
+      fscanf(inFile, "%d", &newPos);
+      fscanf(inFile, "%d", &newTot);
+      fscanf(inFile, "%d", &newDeath);
+      fscanf(inFile, "%d", &currHos);
+      fscanf(inFile, "%d", &currVent);
       
-      if(strcmp(covidCase[i].state, inputS) == 0) {
+      if(strcmp(state, inputS) == 0) {
          //inCoState[j] = covidCase[i];
          
-         strcpy(inCoState[j].date,covidCase[i].date);
-         strcpy(inCoState[j].state,covidCase[i].state);
-         inCoState[j].newPos = covidCase[i].newPos;
-         inCoState[j].newTot = covidCase[i].newTot;
-         inCoState[j].newDeath = covidCase[i].newDeath;
-         inCoState[j].currHos = covidCase[i].currHos;
-         inCoState[j].currVent = covidCase[i].currVent;
+         strcpy(inCoState[i].date,date);
+         strcpy(inCoState[i].state,state);
+         inCoState[i].newPos = newPos;
+         inCoState[i].newTot = newTot;
+         inCoState[i].newDeath = newDeath;
+         inCoState[i].currHos = currHos;
+         inCoState[i].currVent = currVent;
          
-         printf("                   Date: %s\n", inCoState[j].date);
-         printf("                  State: %s\n", inCoState[j].state);         
-         printf("      New positive test: %d tests\n", inCoState[j].newPos);
-         printf("             Total test: %d cases\n", inCoState[j].newTot);
-         printf("              New death: %d deaths\n", inCoState[j].newDeath);
-         printf(" Currently hospitalized: %d cases\n", inCoState[j].currHos);
-         printf("Current on a ventilator: %d cases\n\n", inCoState[j].currVent);
+        //  printf("                   Date: %s\n", inCoState[j].date);
+        //  printf("                  State: %s\n", inCoState[j].state);         
+        //  printf("      New positive test: %d tests\n", inCoState[j].newPos);
+        //  printf("             Total test: %d cases\n", inCoState[j].newTot);
+        //  printf("              New death: %d deaths\n", inCoState[j].newDeath);     //this only works
+        //  printf(" Currently hospitalized: %d cases\n", inCoState[j].currHos);
+        //  printf("Current on a ventilator: %d cases\n\n", inCoState[j].currVent);
          
          
          
-         totalPos += inCoState[j].newPos;
-         totDea += inCoState[j].newDeath;
-         j++;
-         break;
+         totalPos += inCoState[i].newPos;
+         totDea += inCoState[i].newDeath;
+         i++;
+         //break;
       }
-      i++;
    }
    
    fclose(inFile);
    
    //printCaseData(inCoState);
    
-   //SelectionSortCase(inCoState,j);
+   SelectionSortPos(inCoState,i);
    //printf("\n10 most increase case:\n");
    // for( i = 0; i < 3; i++) {
    //    printPlanetData(exoplanets[i]);  
    // }
-//    printf("\n10 most increase case:\n");
-//    for( i = 0; i < 10; i++) {
-//       printCaseData(inCoState[j-i-1]);  
-//    }
+   printf("\nDate with most positive case:\n");
+   for( j = 0; j < 10; j++) {
+      printCaseData(inCoState[i-j-1]);  
+   }
    
-        //  printf("                   Date: %s\n", inCoState[0].date);
-        //  printf("                  State: %s\n", inCoState[0].state);       
-        //  printf("      New positive test: %d tests\n", inCoState[0].newPos);
-        //  printf("             Total test: %d cases\n", inCoState[0].newTot);
-        //  printf("              New death: %d deaths\n", inCoState[0].newDeath);
-        //  printf(" Currently hospitalized: %d cases\n", inCoState[0].currHos);
-        //  printf("Current on a ventilator: %d cases\n\n", inCoState[0].currVent);
-   printf("            Total case: %d\n",totalPos);
-   printf("           Total death: %d\n",totDea);
+   //       printf("                   Date: %s\n", inCoState[i-1].date);
+   //       printf("                  State: %s\n", inCoState[i-1].state);       
+   //       printf("      New positive test: %d tests\n", inCoState[i-1].newPos);
+   //       printf("             Total test: %d cases\n", inCoState[i-1].newTot);
+   //       printf("              New death: %d deaths\n", inCoState[i-1].newDeath);     //this only works
+   //       printf(" Currently hospitalized: %d cases\n", inCoState[i-1].currHos);
+   //       printf("Current on a ventilator: %d cases\n\n", inCoState[i-1].currVent);
+   // printf("            Total case: %d\n",totalPos);
+   // printf("           Total death: %d\n",totDea);
    
    
    
